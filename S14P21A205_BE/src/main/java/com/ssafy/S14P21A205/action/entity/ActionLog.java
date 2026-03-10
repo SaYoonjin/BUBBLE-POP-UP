7c2a1b0ebcaf7ab441e900b4d650396822d84279
정은지 <unji0907@naver.com>
2026-03-10 16:35:21 +0900

    S14P21A205-178 Feat : action 추가

---
+++ S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/action/entity/ActionLog.java
@@ -0,0 +1,38 @@
+package com.ssafy.S14P21A205.action.entity;
+
+import com.ssafy.S14P21A205.store.entity.Store;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "action_log")
+@IdClass(ActionLogId.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ActionLog {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "action_id", nullable = false)
+    private Action action;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(name = "game_day", nullable = false)
+    private Integer gameDay;
+
+    @Column(name = "is_used", nullable = false)
+    private Boolean isUsed;
+}
---
+++ S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/action/entity/ActionLogId.java
@@ -0,0 +1,15 @@
+package com.ssafy.S14P21A205.action.entity;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class ActionLogId implements Serializable {
+
+    private Long action;
+    private Long store;
+}
