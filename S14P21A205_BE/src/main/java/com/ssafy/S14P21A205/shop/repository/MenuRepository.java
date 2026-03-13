c3187710700ed0ff94115228fe41bc2d3cb2b69f
jiwon0226 <ppg0226@naver.com>
2026-03-14 02:57:43 +0900

    S14P21A205-206 Feat : 중복되는 MenuRepository 삭제

--- S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/shop/repository/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.S14P21A205.shop.repository;
-
-import com.ssafy.S14P21A205.shop.entity.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-}
