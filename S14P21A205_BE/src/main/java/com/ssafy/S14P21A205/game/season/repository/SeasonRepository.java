f5147374e8a16e75b18c645d612a7bbcc550cdd5
jiwon0226 <ppg0226@naver.com>
2026-03-13 15:10:01 +0900

    S14P21A205-206 Feat : SeasonRepository - 진행 중 시즌 조회 (findByStatus)

---
+++ S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/game/season/repository/SeasonRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.S14P21A205.game.season.repository;
+
+import com.ssafy.S14P21A205.game.season.entity.Season;
+import com.ssafy.S14P21A205.game.season.entity.SeasonStatus;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeasonRepository extends JpaRepository<Season, Long> {
+
+    Optional<Season> findByStatus(SeasonStatus status);
+}
