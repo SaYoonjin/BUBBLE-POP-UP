b9e0ecdf5261fd0a5e114dbf797c4e95bdf00fa7
jiwon0226 <ppg0226@naver.com>
2026-03-16 11:21:20 +0900

    S14P21A205-208 Feat: ActionService 인터페이스에 POST 메서드 4개 추가

--- S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/action/service/ActionService.java
+++ S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/action/service/ActionService.java
@@ -1,11 +1,23 @@
 package com.ssafy.S14P21A205.action.service;
 
+import com.ssafy.S14P21A205.action.dto.ActionResponse;
 import com.ssafy.S14P21A205.action.dto.ActionStatusResponse;
+import com.ssafy.S14P21A205.action.dto.EmergencyOrderRequest;
+import com.ssafy.S14P21A205.action.dto.EmergencyOrderResponse;
 import com.ssafy.S14P21A205.action.dto.PromotionPriceResponse;
+import com.ssafy.S14P21A205.action.dto.PromotionRequest;
 
 public interface ActionService {
 
     ActionStatusResponse getActionStatus(Integer userId);
 
     PromotionPriceResponse getPromotionPrices();
+
+    ActionResponse executePromotion(Integer userId, PromotionRequest request);
+
+    ActionResponse executeDiscount(Integer userId);
+
+    ActionResponse executeDonation(Integer userId);
+
+    EmergencyOrderResponse executeEmergencyOrder(Integer userId, EmergencyOrderRequest request);
 }
