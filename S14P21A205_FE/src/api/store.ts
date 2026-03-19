63bcadc89d3aa9c8eba26c97ae61dc05a94f4eda
jiwon0226 <ppg0226@naver.com>
2026-03-19 22:19:18 +0900

    S14P21A205-284 Feat: 게임/액션/스토어 API 함수 추가

--- S14P21A205_FE/src/api/store.ts
+++ S14P21A205_FE/src/api/store.ts
@@ -1,3 +1,64 @@
-// import client from "./client";
+import client from "./client";
 
-export default {};
+// --- 타입 정의 ---
+
+export interface StoreResponse {
+  location: string;
+  popupName: string;
+  menu: string;
+  day: number;
+}
+
+export interface MenuItem {
+  menuId: number;
+  menuName: string;
+  ingredientPrice: number;
+  discount: number;
+}
+
+export interface MenuListResponse {
+  menus: MenuItem[];
+}
+
+export interface LocationItem {
+  locationId: number;
+  locationName: string;
+  rent: number;
+  interiorCost: number;
+  discount: number;
+}
+
+export interface LocationListResponse {
+  locations: LocationItem[];
+}
+
+export interface UpdateStoreLocationResponse {
+  locationId: number;
+  balance: number;
+}
+
+// --- API 함수 ---
+
+/** 내 매장 정보 조회 */
+export async function getStore() {
+  const { data } = await client.get<StoreResponse>("/api/stores");
+  return data;
+}
+
+/** 메뉴 목록 조회 */
+export async function getMenuList() {
+  const { data } = await client.get<MenuListResponse>("/api/stores/menus");
+  return data;
+}
+
+/** 지역 목록 조회 */
+export async function getLocationList() {
+  const { data } = await client.get<LocationListResponse>("/api/stores/locations");
+  return data;
+}
+
+/** 매장 지역 변경 */
+export async function updateStoreLocation(locationId: number) {
+  const { data } = await client.patch<UpdateStoreLocationResponse>("/api/stores/location", { locationId });
+  return data;
+}
