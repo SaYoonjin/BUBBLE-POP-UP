f5572489910467e42d79ad75ac9f05f650deed10
정은지 <unji0907@naver.com>
2026-03-19 15:44:11 +0900

    S14P21A205-249 Fix : 순위 조회 api 수정

--- S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/game/news/controller/NewsController.java
+++ S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/game/news/controller/NewsController.java
@@ -3,14 +3,9 @@ package com.ssafy.S14P21A205.game.news.controller;
 import com.ssafy.S14P21A205.game.news.dto.NewsListResponse;
 import com.ssafy.S14P21A205.game.news.dto.NewsRankingResponse;
 import com.ssafy.S14P21A205.game.news.service.NewsService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +20,9 @@ public class NewsController implements NewsControllerDoc {
     @GetMapping("/today")
     public ResponseEntity<NewsListResponse> getTodayNews() {
         return ResponseEntity.ok(newsService.getTodayNews());
-    @Operation(summary = "지역별 매출/유동인구 순위 조회", security = @SecurityRequirement(name = "bearerAuth"))
+    }
+
+    @Override
     @GetMapping("/ranking")
     public ResponseEntity<NewsRankingResponse> getAreaRankings() {
         return ResponseEntity.ok(newsService.getAreaRankings());
