718c9dd968c37709976f9d7a24c1b03e7fc6579a
정은지 <unji0907@naver.com>
2026-03-19 14:29:55 +0900

    S14P21A205-249 Feat : 랭킹 조회 api 추가

--- S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/game/news/controller/NewsController.java
+++ S14P21A205_BE/src/main/java/com/ssafy/S14P21A205/game/news/controller/NewsController.java
@@ -1,10 +1,12 @@
 package com.ssafy.S14P21A205.game.news.controller;
 
+import com.ssafy.S14P21A205.game.news.dto.NewsRankingResponse;
 import com.ssafy.S14P21A205.game.news.service.NewsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class NewsController {
 
     private final NewsService newsService;
+
+    @Operation(summary = "지역별 매출/유동인구 순위 조회", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping("/ranking")
+    public ResponseEntity<NewsRankingResponse> getAreaRankings() {
+        return ResponseEntity.ok(newsService.getAreaRankings());
+    }
 
     @Operation(summary = "시즌 뉴스 생성", security = @SecurityRequirement(name = "bearerAuth"))
     @PostMapping("/generate/{seasonId}")
