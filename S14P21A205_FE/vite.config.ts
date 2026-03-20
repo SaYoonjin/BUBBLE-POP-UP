3fc00ce7ce2e6f9c0dbe11dddcfcb90796f43bf9
양다희 <yangdahee0622@gmail.com>
2026-03-20 11:09:50 +0900

    fix : 영업준비 페이지 api 연동

--- S14P21A205_FE/vite.config.ts
+++ S14P21A205_FE/vite.config.ts
@@ -18,6 +18,10 @@ export default defineConfig({
         target: 'http://localhost:8080',
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
+      '/api/orders': {
+        target: 'http://localhost:8080',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
       '/api': {
         target: 'http://localhost:8080',
       },
