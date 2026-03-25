0294e5db461ad20237949fb8a227a1cae32cf0c2
양다희 <yangdahee0622@gmail.com>
2026-03-25 15:01:05 +0900

    fix : 실시간 알림 수정

--- S14P21A205_FE/src/App.tsx
+++ S14P21A205_FE/src/App.tsx
@@ -1,6 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import BgmController from "./components/common/BgmController";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <BgmController />
+    </>
+  );
 }
