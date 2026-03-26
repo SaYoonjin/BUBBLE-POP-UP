b166359f0ce2cc485faf9a3b38250882f4eeea2e
박솔희 <ppsh1122@naver.com>
2026-03-26 20:09:15 +0900

    [fix]: 브랜치 수정하기

--- S14P21A205_FE/src/pages/PlayPage.tsx
+++ S14P21A205_FE/src/pages/PlayPage.tsx
@@ -2069,6 +2069,13 @@ function PlayPageSession({
             setLiveSellingPrice(response.newPrice);
             syncDiscountActionState(true);
 
+            completeAction("discount", {
+              alert: {
+                title: "할인 이벤트 적용",
+                description: `${rate}% 할인이 적용되었습니다.`,
+              },
+            });
+
             const [stateResult] = await Promise.allSettled([getGameDayState()]);
 
             if (stateResult.status === "fulfilled") {
@@ -2096,12 +2103,6 @@ function PlayPageSession({
               });
             }
 
-            completeAction("discount", {
-              alert: {
-                title: "할인 이벤트 적용",
-                description: `${rate}% 할인이 적용되었습니다.`,
-              },
-            });
           }}
         />
       )}
